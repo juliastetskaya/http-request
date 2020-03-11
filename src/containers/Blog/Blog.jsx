3600afc4a602cca541c0adb85bfd6ab5b8486451
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2020-03-11 14:24:03 +0300

    prepare the project for routing

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,31 +1,10 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from '../Posts/Posts';
 import './Blog.css';
 
 const Blog = () => {
-    const [posts, setPosts] = useState([]);
-    const [error, setError] = useState(false);
     const [selectedPostId, setSelectedPostId] = useState(null);
-    const getPosts = useCallback(() => {
-        axios.get('/posts')
-            .then(response => {
-                setPosts(response.data.slice(0, 4).map(post => ({
-                    ...post,
-                    author: 'Max',
-                })));
-            })
-            .catch(() => {
-                setError(true);
-            })
-    }, [setPosts]);
-
-    useEffect(() => {
-        getPosts();
-    }, [getPosts]);
 
     const postSelectedHandler = (id) => {
         setSelectedPostId(id);
@@ -41,24 +20,9 @@ const Blog = () => {
                     </ul>
                 </nav>
             </header>
-            <section className="Posts">
-                {!error ? posts.map(({ id, title, author }) =>
-                    <Post
-                        key={id}
-                        title={title}
-                        author={author}
-                        clickHandler={() => postSelectedHandler(id)}
-                    />)
-                : <p style={{ textAlign: 'center', color: 'red'}}>Something went wrong!</p>}
-            </section>
-            <section>
-                <FullPost id={selectedPostId} />
-            </section>
-            <section>
-                <NewPost />
-            </section>
+            <Posts postSelectedHandler={postSelectedHandler} />
         </div>
-    )
+    );
 };
 
 export default Blog;
---
+++ src/containers/Posts/Posts.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import './Posts.css';
+
+const Posts = ({ postSelectedHandler }) => {
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(false);
+    
+    const getPosts = useCallback(() => {
+        axios.get('/posts')
+            .then(response => {
+                setPosts(response.data.slice(0, 4).map(post => ({
+                    ...post,
+                    author: 'Max',
+                })));
+            })
+            .catch(() => {
+                setError(true);
+            })
+    }, [setPosts]);
+
+    useEffect(() => {
+        getPosts();
+    }, [getPosts]);
+
+    return (
+        <section className="Posts">
+            {!error ? posts.map(({ id, title, author }) =>
+                <Post
+                    key={id}
+                    title={title}
+                    author={author}
+                    clickHandler={() => postSelectedHandler(id)}
+                />)
+            : <p style={{ textAlign: 'center', color: 'red'}}>Something went wrong!</p>}
+        </section>
+    );
+};
+
+export default Posts;
