0a082562495676fe2e30c47559205578b352560d
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2020-03-11 14:46:48 +0300

    use Links to switch pages

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
@@ -10,8 +10,8 @@ const Blog = () => (
         <header className="Blog">
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/new-post">New Post</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/new-post">New Post</Link></li>
                 </ul>
             </nav>
         </header>
