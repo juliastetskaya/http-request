bb9544120762a073473309173e96c1cc1970eac3
Julia Stetskaya <juliastetskaya@gmail.com>
2020-03-13 11:23:40 +0300

    redirect requests

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
@@ -18,6 +18,7 @@ const Blog = () => (
         <Switch>
             <Route path="/posts/" component={Posts} />
             <Route path="/new-post" component={NewPost} />
+            <Redirect from="/" to="/posts" />
         </Switch>
     </div>
 );
